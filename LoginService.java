6da9e206c01d2fe45918c0718093b24b44d8ef12
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-09 12:11:00 +0100

    Create LoginService.java

---
+++ LoginService.java
@@ -0,0 +1,6 @@
+public class LoginService {
+    public void connect() {
+        String password = "P@ssw0rd123";  // Hardcoded password
+        System.out.println("Connecting with password: " + password);
+    }
+}
