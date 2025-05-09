69798e90364b92f6735b049c7dd8edb95e4e6066
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-09 13:23:28 +0100

    Delete LoginService.java

--- LoginService.java
+++
@@ -1,6 +0,0 @@
-public class LoginService {
-    public void connect() {
-        String password = "P@ssw0rd123";  // Hardcoded password
-        System.out.println("Connecting with password: " + password);
-    }
-}
